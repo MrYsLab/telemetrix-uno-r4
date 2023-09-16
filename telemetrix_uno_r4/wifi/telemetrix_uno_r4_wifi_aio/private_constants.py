69894a77af27ba390d29a1436bb5ea5f486d7e68
MrYsLab <MisterYsLab@gmail.com>
2023-09-16 19:21:55 -0400

    Re-enabled servo for BLE

--- telemetrix_uno_r4/wifi/telemetrix_uno_r4_wifi_aio/private_constants.py
+++ telemetrix_uno_r4/wifi/telemetrix_uno_r4_wifi_aio/private_constants.py
@@ -111,7 +111,7 @@ class PrivateConstants:
     FEATURES = 20
     DEBUG_PRINT = 99
 
-    TELEMETRIX_VERSION = "1.01"
+    TELEMETRIX_VERSION = "1.02"
 
     # reporting control
     REPORTING_DISABLE_ALL = 0
